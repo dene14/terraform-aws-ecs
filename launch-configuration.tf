97a0f6b2e530115359498bc657f35014b9c9afbc
Denis Boulas <dene14@gmail.com>
2016-08-07 15:40:56 +0300

    Added subnet_ids export.

--- launch-configuration.tf
+++ launch-configuration.tf
@@ -6,5 +6,5 @@ resource "aws_launch_configuration" "ecs" {
     key_name = "${var.key_name}"
     security_groups = ["${split(",", var.security_group_ids)}"]
     user_data = "#!/bin/bash\necho ECS_CLUSTER=${var.cluster_name} > /etc/ecs/ecs.config && echo ECS_ENGINE_AUTH_TYPE=dockercfg >> /etc/ecs/ecs.config && echo ECS_ENGINE_AUTH_DATA='{\"${var.registry_url}\":{\"auth\":\"${var.registry_auth}\",\"email\":\"${var.registry_email}\"}}' >> /etc/ecs/ecs.config"
-    associate_public_ip_address = true
+#    associate_public_ip_address = true
 }
